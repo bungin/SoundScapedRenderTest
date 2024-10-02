3be28cf4c21cbc78600bdb3c1eba974640c66c76
denisetruuong <denise.truong1124@gmail.com>
2024-10-01 20:57:32 -0500

    added more codes

--- server/src/routes/api/lastfm.ts
+++ server/src/routes/api/lastfm.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from "express";
 import axios from "axios";
+import crypto from "crypto";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+// // Last.fm API root URL
+// Documentation: http://ws.audioscrobbler.com/2.0/
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -23,4 +27,40 @@ app.get("/callback", async (req: Request, res: Response) => {
   const token = req.query.token as string;
   const apiKey = process.env.LASTFM_API_KEY;
   const apiSecret = process.env.LASTGM_API_SECRET;
+
+  const apiSig = crypto
+    .createHash("md5")
+    .update(`api_key${apiKey}methodauth.getSessiontoken${token}${apiSecret}`)
+    .digest("hex");
+
+  try {
+    const response = await axios.get(
+      `http://ws.audioscrobbler.com/2.0/?method=auth.getSession&api_key=${apiKey}&token=${token}&api_sig=${apiSig}&format=json`
+    );
+    sessionKey = response.data.session.key;
+    res.sendFile(__dirname + "/../public/search.html");
+  } catch (error) {
+    res.status(404).send("Error fetching session key");
+  }
+});
+
+// searching songs endpoint
+app.post("/search", async (req: Request, res: Response) => {
+  const { songName } = req.body;
+  const apiKey = process.env.LASTFM_API_KEY;
+
+  try {
+    const response = await axios.get(
+      `http://ws.audioscrobbler.com/2.0/?method=track.search&track=${encodeURIComponent(
+        songName
+      )}&api_key=${apiKey}&format=json`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send("Error searching for songs");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
