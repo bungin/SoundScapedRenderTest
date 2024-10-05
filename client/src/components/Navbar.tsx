a870ebf424f217a0bbbf955bebd2c8a5a682f372
bungin <nimai.band@gmail.com>
2024-10-05 04:24:35 -0500

    copied+edited login ascii to loggedIn logic, made eyes open. adjusted bg2/stars, less fuzzy

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -19,31 +19,45 @@ const Navbar = () => {
   }, [loginCheck]);
   return (
     <div>
-    <div className="navBG">
-      <h1 className="center">SoundScaped</h1>
-      <div>
-        {!loginCheck ? (
-          <div className="">
-            <p className="center" style={{fontSize:'20px'}}>
-              //--------------------------\\<br/>//\__//\__//d--b//\__//\__/\\<br/>\----------------------------/</p>
-            {/* display: 'none' to hide unnecesary btn */}
-          <Button style={{display:'none'}}>
-            <Link to="/login">Login</Link>
-          </Button>
-          </div>
-        ) : (
-          <Button
-            className="outButton"
-            onClick={() => {
-              auth.logout();
-            }}
-          >
-            Logout
-          </Button>
-        )}
+      <div className="navBG">
+        <h1 className="center">SoundScaped</h1>
+        <div>
+          {!loginCheck ? (
+            <div className="">
+              <p className="center" style={{ fontSize: "20px" }}>
+                //--------------------------\\
+                <br />
+                //\__//\__//d--b//\__//\__/\\
+                <br />
+                \----------------------------/
+              </p>
+              {/* display: 'none' to hide unnecesary btn */}
+              <Button style={{ display: "none" }}>
+                <Link to="/login">Login</Link>
+              </Button>
+            </div>
+          ) : (
+            <>
+              <p className="center" style={{ fontSize: "20px" }}>
+                //--------------------------\\
+                <br />
+                //\__//\__//d**b//\__//\__/\\
+                <br />
+                \----------------------------/
+              </p>
+              <Button
+                className="outButton"
+                onClick={() => {
+                  auth.logout();
+                }}
+              >
+                Logout
+              </Button>
+            </>
+          )}
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
