65c7a899f210a31194616c092767cc65874f4c7d
bungin <nimai.band@gmail.com>
2024-10-06 05:13:32 -0500

    port10

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: "0.0.0.0",
-    strictPort: true,
+    strictPort: false,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
