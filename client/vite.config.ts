ddbc69463cd5d78142f4c797010e0ed6ec37eb0d
bungin <nimai.band@gmail.com>
2024-10-06 04:27:00 -0500

    port 7

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,12 +11,12 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:8001',
         changeOrigin: true,
         secure: false,
       },
       '/auth': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:8001',
         changeOrigin: true,
         secure: false,
       },
